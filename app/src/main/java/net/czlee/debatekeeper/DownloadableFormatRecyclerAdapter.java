e9ef9a73d3840d3b152deb9c8ea19335859fb770
Chuan-Zheng Lee <czlee@alumni.stanford.edu>
2021-10-05 18:05:03 +1300

    Clean up copyright notices

--- app/src/main/java/net/czlee/debatekeeper/DownloadableFormatRecyclerAdapter.java
+++ app/src/main/java/net/czlee/debatekeeper/DownloadableFormatRecyclerAdapter.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright (C) 2021 Chuan-Zheng Lee
+ *
+ * This file is part of the Debatekeeper app, which is licensed under the GNU
+ * General Public Licence version 3 (GPLv3).  You can redistribute and/or modify
+ * it under the terms of the GPLv3, and you must not use this file except in
+ * compliance with the GPLv3.
+ *
+ * This app is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE.  See the GNU General Public Licence for more details.
+ *
+ * You should have received a copy of the GNU General Public Licence along with
+ * this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package net.czlee.debatekeeper;
 
 import android.text.TextUtils;
@@ -13,6 +29,9 @@ import net.czlee.debatekeeper.databinding.ViewFormatDownloadBinding;
 
 /**
  * {@link RecyclerView.Adapter} that can display a {@link DownloadableFormatEntry}.
+ *
+ * @author Chuan-Zheng Lee
+ * @since 2021-09-29
  */
 public class DownloadableFormatRecyclerAdapter extends RecyclerView.Adapter<DownloadableFormatRecyclerAdapter.ViewHolder> {
 
